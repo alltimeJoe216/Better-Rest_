a331085e1ed19ff886d2866912dafe5c60603139
Joe V <joeveverka89@gmail.com>
2020-08-28 12:34:44 -0400

    Complete Project.

---
+++ BetterRest/ML.playground/Contents.swift
@@ -0,0 +1,15 @@
+import CreateML
+import Foundation
+
+let data = try MLDataTable(contentsOf: URL(fileURLWithPath: "Users/joeveverka/Desktop/better/better-rest.json"))
+let (trainingData, testingData) = data.randomSplit(by: 0.8)
+
+let regressor = try MLRegressor(trainingData: trainingData, targetColumn: "actualSleep")
+
+let evaluationMetrics = regressor.evaluation(on: testingData)
+print(evaluationMetrics.rootMeanSquaredError)
+print(evaluationMetrics.maximumError)
+
+let metadata = MLModelMetadata(author: "Daddy", shortDescription: "A model to prdict sleep time for coffee drinks", license: nil, version: "1.0")
+
+try regressor.write(to: URL(fileURLWithPath: "/users/joeveverka/desktop/sleepcalculator.mlmodel"), metadata: metadata)
